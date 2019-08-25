579166b1e2fba6bbe570c66c2d25a5e01e7a268b
Zaursk82 <zaursemail@gmail.com>
2019-08-24 18:42:30 -0700

    myfirst commit

---
+++ src/test/java/Myproject/AppiumTest.java
@@ -0,0 +1,18 @@
+package Myproject;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidApp() 
+	{
+		System.out.println("AndroidApp");
+	}
+	@Test
+	public void IOSapp() 
+	{
+		System.out.println("IOSapp");
+	}
+
+}
---
+++ src/test/java/Myproject/SeleniumTest.java
@@ -0,0 +1,17 @@
+package Myproject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation() 
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void ElementUI() 
+	{
+		System.out.println("ElementUI");
+	}
+}
